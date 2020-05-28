4c77ff52a1ff6fcbe3bcc83d8c976514d8601c79
wasamisam0119 <zhaoshujia0123@gmail.com>
2020-05-28 10:51:38 +0100

    first commit

---
+++ Assets/myRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class myRotate : MonoBehaviour
+{
+
+    // Update is called once per frame
+    float rotSpeed =  20;
+    void OnMouseDrag() {
+        float rotX = Input.GetAxis("Mouse X")*rotSpeed*Mathf.Deg2Rad;   
+        float rotY = Input.GetAxis("Mouse Y")*rotSpeed*Mathf.Deg2Rad;   
+
+        transform.RotateAround(Vector3.up,-rotX);
+        transform.RotateAround(Vector3.right,rotY);
+    }
+}
---
+++ Assets/rotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Space rotationSpace;
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKeyDown(KeyCode.R))
+        transform.Rotate(new Vector3(0,10,0),rotationSpace);
+        
+    }
+}
